4894d891a57e456b891dcc27b4a1a3af87c64d66
clarissa2 <clarissa.auckenthaler@gmail.com>
2022-10-30 23:13:34 +0100

    Task 2.1 and 2.3

--- src/GlobalAligner_Auckenthaler_Dittschar.java
+++ src/GlobalAligner_Auckenthaler_Dittschar.java
@@ -3,6 +3,7 @@ package assignment02;
 
 import java.io.IOException;
 import assignment01.FastA_Auckenthaler_Dittschar;
+import java.util.Arrays;
 /**
  * GlobalAligner_Auckenthaler_Dittschar
  * Autors: Clarissa Auckenthaler & Marina Dittschar
@@ -14,16 +15,22 @@ public class GlobalAligner_Auckenthaler_Dittschar {
 			throw new IOException("Usage: GlobalAligner_Auckenthaler_Dittschar infile [quadraticSpace|linearSpace|noDP]");
 
 		var list=FastA_Auckenthaler_Dittschar.read(args[0]);
+
 		runNeedlemanWunschQuadraticSpace(list.get(0), list.get(1));
+
+		runNeedlemanWunschRecursively(list.get(0),list.get(1));
+
+
 		if(list.size()!=2)
 			throw new IOException("Wrong number of input sequences: "+list.size());
 
-		var mode=(args.length==2?args[1]:"quadraticSpace");
+
+		var mode=(args.length==2?args[1]:"linearSpace");
 
 		switch(mode) {
-			case "quadraticSpace" -> runNeedlemanWunschQuadraticSpace(list.get(0),list.get(1));
-			case "linearSpace" ->runNeedlemanWunschLinearSpace(list.get(0),list.get(1));
-			case "noDP" ->runNeedlemanWunschQuadraticSpace(list.get(0),list.get(1));
+			case "quadraticSpace" -> runNeedlemanWunschQuadraticSpace(list.get(1),list.get(0));
+			case "linearSpace" ->runNeedlemanWunschLinearSpace(list.get(1),list.get(0));
+			case "noDP" ->runNeedlemanWunschQuadraticSpace(list.get(1),list.get(0));
 			default -> throw new IOException("Unknown mode: "+mode);
 		}
 	}
@@ -44,37 +51,74 @@ public class GlobalAligner_Auckenthaler_Dittschar {
 		int mismatch = -1;
 		int gap = 1;
 
+		Integer[][] nw_matrix = new Integer[xchar.length+1][ychar.length+1];
+		Integer[][] traceback_matrix = new Integer[xchar.length+1][ychar.length+1];
 
-		Integer[][] nw_matrix = new Integer[xchar.length][ychar.length];
-		Integer[][] traceback_matrix = new Integer[xchar.length][ychar.length];
-		for (int i=0; i<xchar.length; i++){
-			nw_matrix[i][0] = -i;
+		String alignedseq1 = "";
+		String mid = "";
+		String alignedseq2 = "";
+
+		nw_matrix[0][0]= 0;
+
+		for (int i=1; i<xchar.length+1; i++){
+			nw_matrix[i][0] = nw_matrix[i-1][0]-gap;
 		}
-		for (int j=0; j<xchar.length; j++){
-			nw_matrix[0][j] = -j;
+		for (int j=1; j<ychar.length+1; j++){
+			nw_matrix[0][j] = nw_matrix[0][j-1]-gap;
 		}
 		int up = 0;
 		int left = 0;
 		int diagonal = 0;
 		// matrix filling
-		for (int i=1; i<xchar.length;i++){
-			for (int j=1; j<ychar.length; j++){
+		for (int i=1; i<xchar.length+1;i++){
+			for (int j=1; j<ychar.length+1; j++){
 
-				if (xchar[i] == ychar[j]){
-					diagonal = match ;
+				if (xchar[i-1] == ychar[j-1]){
+					diagonal = nw_matrix[i-1][j-1]+ match;
 				}
 				else{
-					diagonal = mismatch;
+					diagonal = nw_matrix[i-1][j-1] +mismatch;
 				}
 				up = nw_matrix[i-1][j] - gap;
 				left = nw_matrix[i][j-1] - gap;
 				nw_matrix[i][j] = Math.max(up, Math.max(left, diagonal));
+				//creating tracebackmatrix
 				Integer[] traceback_arr = {up, left, diagonal};
-				// no function named argmax - we'll have to write our own
-				// traceback_matrix[i][j] = argmax(traceback_arr);
+				int tracer = argmax(traceback_arr);
+				traceback_matrix[i][j]= tracer;
+
 			}
 		}
+
 		prettyPrint(nw_matrix);
+
+		//generating alignment sequence
+		int i = traceback_matrix.length-1;
+		int j = traceback_matrix[0].length-1;
+
+		while (i!= 0 && j!= 0){
+			if (traceback_matrix[i][j]==2){
+				alignedseq1 = xchar[i-1]+ alignedseq1;
+				alignedseq2 = ychar[j-1] + alignedseq2;
+				i -= 1;
+				j -= 1;
+			}
+			else if(traceback_matrix[i][j]==1){
+				alignedseq1 =  "-" + alignedseq1;
+				alignedseq2 = ychar[j-1]+ alignedseq2;
+
+				j -= 1;
+
+			}
+			else{
+				alignedseq1 =  xchar[i-1] + alignedseq1;
+				alignedseq2 = "-" + alignedseq2;
+				i -= 1;
+			}
+		}
+		System.out.println("The alignment score is " + nw_matrix[nw_matrix.length-1][nw_matrix[0].length-1]);
+		System.out.println(alignedseq1);
+		System.out.println(alignedseq2);
 
 	}
 
@@ -87,6 +131,11 @@ public class GlobalAligner_Auckenthaler_Dittschar {
 	 */
 	public static void runNeedlemanWunschLinearSpace(FastA_Auckenthaler_Dittschar.Pair x, FastA_Auckenthaler_Dittschar.Pair y) {
 		// todo: implement, Assignment 2.2
+		char[] xchar = x.sequence().toCharArray();
+		char[] ychar = y.sequence().toCharArray();
+		int match = 1;
+		int mismatch = -1;
+		int gap = 1;
 	}
 
 	/**
@@ -98,7 +147,49 @@ public class GlobalAligner_Auckenthaler_Dittschar {
 	 */
 	public static void runNeedlemanWunschRecursively(FastA_Auckenthaler_Dittschar.Pair x, FastA_Auckenthaler_Dittschar.Pair y) {
 		// todo: implement using recursive function computeF, , Assignment 2.3
+		char[] xchar = x.sequence().toCharArray();
+		char[] ychar = y.sequence().toCharArray();
+		int match = 1;
+		int mismatch = -1;
+		int gap = 1;
+		int matchscore= -1;
+
+		int xlength = xchar.length;
+		int ylength = ychar.length;
+		System.out.println(xlength);
+		System.out.println(ylength);
+		int[][] maxAlign = new int[xlength+1][ylength+1];
+
+		for(int i = 0; i < xlength+1; i++) {
+			for( int j= 0; j< ylength+1; j++){
+				if(i == 0 && j== 0){
+					maxAlign[i][j]= 0;
+				}
+				else if (i== 0 && j!=0)
+					maxAlign[i][j]= -j * gap;
+				else if (i!= 0 && j==0)
+					maxAlign[j][i] = -i* gap;
+
+				else{
+					if (xchar[i-1] == ychar[j-1]){
+						matchscore =  match;
+					}
+					else{
+						matchscore = mismatch;
+					}
+
+					int top_left= maxAlign[i-1][j-1]+ matchscore;
+					int top = maxAlign[i][j-1]+gap;
+					int left = maxAlign[i-1][j]+gap;
+
+					maxAlign[i][j]= Math.max(top_left, Math.max(top, left));
+				}
+			}
+		}
+		System.out.println("runNeedlemanWunschRecursively alignment score: "+maxAlign[xlength][ylength]);
 	}
+
+
 	public static void prettyPrint(Integer[][] nw_matrix){
 		for(Integer[] i : nw_matrix) {
 			for(int j : i) {
@@ -108,6 +199,17 @@ public class GlobalAligner_Auckenthaler_Dittschar {
 			// new row
 			System.out.println();
 		}
+	}
+	public static Integer argmax(Integer[] array) {
+		int re = Integer.MIN_VALUE;
+		int arg = -1;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > re) {
+				re = array[i];
+				arg = i;
+			}
+		}
+		return arg;
 	}
 
 	public static int computeF(int i,int j) {
